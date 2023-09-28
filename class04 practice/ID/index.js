219dbd49eb883ca55b088350ceb62291ebc49cc5
Navneet Aulakh <navneetaulakh@Navneets-MBP.hitronhub.home>
2023-09-27 20:52:14 -0700

    adding class04 practice folder

---
+++ class04 practice/ID/index.js
@@ -0,0 +1,31 @@
+/**
+ * Incrementing 
+ */
+var number = 0;
+console.log(number);
+
+number = number + 1
+console.log(number);
+
+number += 1;
+console.log(number);
+
+number++;
+console.log(number);
+
+
+/**
+ * Decrementing
+ */
+var value = 100;
+console.log(value);
+
+value = value - 1;
+console.log(value);
+
+value -= 1;
+console.log(value);
+
+value --;
+console.log(value);
+
